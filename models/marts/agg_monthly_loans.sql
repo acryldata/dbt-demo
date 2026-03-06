acryldata/dbt-demo

commit f9d33f8c8e54a1d46de01ea7a6410cb6072e40ca

Add customer_id to monthly loan aggregations
Added customer_id field to the monthly loan summary report by joining to the loans table. This will help us track which customers are associated with each month's loan activity.

models/marts/agg_monthly_loans.sql
@@ -33,6 +33,7 @@ combined as (
     select
         coalesce(orig.month_start, pay.month_start) as month,
         orig.loan_type_name,
+        loans.customer_id,
         coalesce(orig.loans_originated, 0) as new_loans,
         coalesce(orig.total_amount_originated, 0) as amount_originated,
         coalesce(orig.avg_loan_amount, 0) as avg_loan_size,
@@ -44,6 +45,8 @@ combined as (
     from monthly_originations orig
     full outer join monthly_payments pay
         on orig.month_start = pay.month_start
+    left join loans
+        on orig.loan_type_name = loans.loan_type_name
 )
 
 select * from combined

commit aaa075715e54f9be4980e24be28f76291a8ec152

Fix: Remove join fan-out bug in agg_monthly_loans
Removes the problematic left join on loans table that was causing
aggregated loan amounts to be multiplied by the number of loans per
loan_type. This join was creating a Cartesian product that inflated
the amount_originated values.

The customer_id field was not being used in the aggregation and was
causing the data quality issue reported in the Risk Analytics
Reporting Dashboard.

Fixes the issue introduced in commit f9d33f8.

models/marts/agg_monthly_loans.sql
@@ -33,7 +33,6 @@ combined as (
     select
         coalesce(orig.month_start, pay.month_start) as month,
         orig.loan_type_name,
-        loans.customer_id,
         coalesce(orig.loans_originated, 0) as new_loans,
         coalesce(orig.total_amount_originated, 0) as amount_originated,
         coalesce(orig.avg_loan_amount, 0) as avg_loan_size,
@@ -45,8 +44,6 @@ combined as (
     from monthly_originations orig
     full outer join monthly_payments pay
         on orig.month_start = pay.month_start
-    left join loans
-        on orig.loan_type_name = loans.loan_type_name
 )
 
 select * from combined